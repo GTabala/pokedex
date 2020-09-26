e8bf97edbc1d234e0716afb9a277e0263fd73a82
Gennady Tabala <gtabala@gmail.com>
2020-09-26 21:21:33 +0100

    Indents

--- src/BestPokemon.js
+++ src/BestPokemon.js
@@ -1,17 +1,17 @@
-import React from "react"
+import React from "react";
 
-let BestPokemon = ({abilities}) => {
-    // let  = ["Anticipation", "Adaptability", "Run-Away"];
-    return (
-      <div>
-        <p> My favourite Pokemon is Squirtle</p>
-        <ul>
-          {abilities.map((item, ind) => (
-            <li key={ind}>{item}</li>
-          ))}
-        </ul>
-      </div>
-    );
-  };
+let BestPokemon = ({ abilities }) => {
+  // let  = ["Anticipation", "Adaptability", "Run-Away"];
+  return (
+    <div>
+      <p> My favourite Pokemon is Squirtle</p>
+      <ul>
+        {abilities.map((item, ind) => (
+          <li key={ind}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-  export default BestPokemon;
+export default BestPokemon;
--- src/CaughtPokemon.js
+++ src/CaughtPokemon.js
@@ -1,14 +1,21 @@
-import React, {useState} from "react"
+import React, { useState } from "react";
 import BestPokemon from "./BestPokemon";
-let CaughtPokemon = ({date, abilities1}) => {
-  
-    // let date = new Date().toLocaleDateString();
-const [caught,setCaught] = useState(abilities1);
-const catchPokemon = () => setCaught(caught.concat(abilities1[Math.floor(Math.random()*abilities1.length)]));
-    return (<><BestPokemon abilities = {caught}/>
-    <p>Caught {caught.length} Pokemon on {date}</p>
-    <button onClick={catchPokemon}>Catch it!</button>
-     </> )
-  };
+let CaughtPokemon = ({ date, abilities1 }) => {
+  // let date = new Date().toLocaleDateString();
+  const [caught, setCaught] = useState(abilities1);
+  const catchPokemon = () =>
+    setCaught(
+      caught.concat(abilities1[Math.floor(Math.random() * abilities1.length)])
+    );
+  return (
+    <>
+      <BestPokemon abilities={caught} />
+      <p>
+        Caught {caught.length} Pokemon on {date}
+      </p>
+      <button onClick={catchPokemon}>Catch it!</button>
+    </>
+  );
+};
 
-  export default CaughtPokemon;
+export default CaughtPokemon;
